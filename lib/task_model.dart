ab57bce3c63540e55d59d78ec7b3a52604456f69
Ankitha Yamini <ankithayamini@gmail.com>
2023-11-21 15:20:35 +0530

    Update task_model.dart

--- lib/task_model.dart
+++ lib/task_model.dart
@@ -4,18 +4,18 @@ import 'package:parse_server_sdk/parse_server_sdk.dart';
 class Task {
   String title;
   String description;
-  String objectId; // Make sure objectId is included in your model
+  String objectId;
 
   Task({
     required this.title,
     required this.description,
-    this.objectId = '', // Optional parameter with a default value
+    this.objectId = '',
   });
 
   // Factory method to create a Task from ParseObject
   factory Task.fromParse(ParseObject parseObject) {
     return Task(
-      title: parseObject.get('title') ?? '', // Use the ?? operator for null safety
+      title: parseObject.get('title') ?? '',
       description: parseObject.get('description') ?? '',
       objectId: parseObject.objectId ?? '',
     );
